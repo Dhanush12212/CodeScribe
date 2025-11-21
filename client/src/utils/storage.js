9dc7b9867f08b2f2ce1cdd4e4823b9e9843ecf75
Dhanush <dhanush12212@gmail.com>
2025-11-21 13:42:02 +0530

    Created Storage file

---
+++ client/src/utils/storage.js
@@ -0,0 +1,34 @@
+ 
+//Session Storage
+export const Session = {
+  set(key, value) {
+    const tabKey = `${window.name}-${key}`;
+    sessionStorage.setItem(tabKey, JSON.stringify(value));
+  },
+
+  get(key) {
+    const tabKey = `${window.name}-${key}`;
+    const value = sessionStorage.getItem(tabKey);
+    return value ? JSON.parse(value) : null;
+  },
+
+  remove(key) {
+    const tabKey = `${window.name}-${key}`;
+    sessionStorage.removeItem(tabKey);
+  }
+};
+
+
+//Local Storage
+export const Local = {
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get(key) {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  },
+  remove(key) {
+    localStorage.removeItem(key);
+  }
+};
